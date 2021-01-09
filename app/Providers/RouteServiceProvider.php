4db80efb0c489d0ea68a1b45b49bbd88dd496d11
denyhanif <36994300+denyhanif@users.noreply.github.com>
2021-01-09 08:42:56 +0700

    redirect close page

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/Administrator/home';
+    public const HOME = '/Administrator/home';//awalnya /homre
 
     /**
      * Define your route model bindings, pattern filters, etc.
